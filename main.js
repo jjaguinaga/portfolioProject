09ba64d9308da192d5fa281dae81e79b723584cb
Myself Desktop <juan@aguinaga.us>
2024-05-28 21:57:51 -0500

    blur to a tags

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ box.forEach(function(box) {
     box.style.boxShadow = "none";
   })
 })
+
+const hightlight = document.querySelectorAll("div.spacePic");
+
+hightlight.forEach(function(hightlight) {
+    hightlight.addEventListener("mouseover", function() {
+        hightlight.style.filter = "blur(2px)";
+    })
+    hightlight.addEventListener("mouseout", function() {
+        hightlight.style.filter = "none";
+    })
+})
