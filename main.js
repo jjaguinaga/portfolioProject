1e9defb2bcc452e3f90314d24cd4d91cba76b0f5
Juan Aguinaga <juan_a03@icloud.com>
2024-05-29 22:28:39 -0500

    blur added to a tag

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const box = document.querySelectorAll("div.longBox, div.smallBox");
 
 const createShadow = box.forEach(function(box) {
-  box.addEventListener("mouseover", function() {
-    box.style.boxShadow = "4px 4px 10px purple";
-  })
-  box.addEventListener("mouseout", function() {
-    box.style.boxShadow = "none";
-  })
+   box.addEventListener("mouseover", function() {
+      box.style.boxShadow = "4px 4px 10px purple";
+   })
+   box.addEventListener("mouseout", function() {
+      box.style.boxShadow = "none";
+   })
 })
 
-const hightlight = document.querySelectorAll("div.spacePic");
+const hightlight = document.querySelectorAll("a.linkBox");
 
 const makeBlur = hightlight.forEach(function(hightlight) {
-    hightlight.addEventListener("mouseover", function() {
-        hightlight.style.filter = "blur(2px)";
-    })
-    hightlight.addEventListener("mouseout", function() {
-        hightlight.style.filter = "none";
-    })
+   hightlight.addEventListener("mouseover", function() {
+      hightlight.style.backdropFilter = "blur(3px)";
+   })
+   hightlight.addEventListener("mouseout", function() {
+      hightlight.style.backdropFilter = "none";
+   })
 })
-
