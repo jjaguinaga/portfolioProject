7856680e83df700d8c0df582063a87956ad30208
Juan Aguinaga <juan_a03@icloud.com>
2024-05-30 12:16:58 -0500

    no change

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-const box = document.querySelectorAll("div.longBox, div.smallBox");
-
-const createShadow = box.forEach(function(box) {
-   box.addEventListener("mouseover", function() {
-      box.style.boxShadow = "4px 4px 10px purple";
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+if (mediaQuery.matches) {
+  
+} else  {
+  const box = document.querySelectorAll("div.longBox, div.smallBox");
+   const createShadow = box.forEach(function(box) {
+      box.addEventListener("mouseover", function() {
+         box.style.boxShadow = "4px 4px 10px purple";
+      })
+      box.addEventListener("mouseout", function() {
+         box.style.boxShadow = "none";
+      })
    })
-   box.addEventListener("mouseout", function() {
-      box.style.boxShadow = "none";
-   })
-})
 
-const hightlight = document.querySelectorAll("a.linkBox");
-
-const makeBlur = hightlight.forEach(function(hightlight) {
-   hightlight.addEventListener("mouseover", function() {
-      hightlight.style.backdropFilter = "blur(3px)";
+   const hightlight = document.querySelectorAll("a.linkBox");
+   const makeBlur = hightlight.forEach(function(hightlight) {
+      hightlight.addEventListener("mouseover", function() {
+         hightlight.style.backdropFilter = "blur(3px)";
+      })
+      hightlight.addEventListener("mouseout", function() {
+         hightlight.style.backdropFilter = "none";
+      })
    })
-   hightlight.addEventListener("mouseout", function() {
-      hightlight.style.backdropFilter = "none";
-   })
-})
 
-function openWindow1()
-{
-var URL = "please specify your URL";
-window.open(URL,"RecoverPassword","width=700,height=450");
+   function openWindow1() {
+   var URL = "please specify your URL";
+   window.open(URL,"RecoverPassword","width=700,height=450");
+   } 
 }
